4a817ffdb1c50a6f92053c7ed7532326e544e438
Par Joona <parjoona@hotmail.com>
2019-10-07 12:32:01 +0200

    hopefully added request object on apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,26 @@ import UserHandler from './store';
 import AppNavigator from './navigators';
 import ApolloClient, {InMemoryCache} from 'apollo-boost';
 import {ApolloProvider} from '@apollo/react-hooks';
+import SInfo from 'react-native-sensitive-info';
+
+import {REACT_APP_DEVELOPMENT} from 'react-native-dotenv';
 
 const cache = new InMemoryCache({
   dataIdFromObject: obj => obj.id,
 });
 
 const client = new ApolloClient({
+  uri: REACT_APP_DEVELOPMENT,
   cache,
+  request: async op => {
+    const token = await SInfo.getItem('accessToken', {});
+    op.setContext(context => ({
+      headers: {
+        ...context.headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }));
+  },
 });
 
 export default () => (
