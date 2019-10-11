fe13e89ec7a69ea5f85a2cf4fb98c93938dcf7c8
birkas <birkas@netcompany.com>
2019-10-11 14:40:25 +0200

    fixed return of timestamps for users

--- server/app/extensions.py
+++ server/app/extensions.py
@@ -7,6 +7,7 @@ def prepare(object):
     """Converts _id from ObjectId to string."""
     print(object)
     object['_id'] = str(object['_id'])
+    
     return object
 
 def get_user(nickname, email, mongo):
