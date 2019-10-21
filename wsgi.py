39467b8efadb32f6f585d066275a153d940f738c
birkas <birkas@netcompany.com>
2019-10-21 13:02:46 +0200

    implemented logging for gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 from app.server import create_app
+import logging
 import os
 
 app = create_app()
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 if "FLASK_ENV" in os.environ and os.environ["FLASK_ENV"] is "development": 
     app.run()
